abaf80236e2febc6c672686766f1745148b2b594
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2023-03-18 15:26:38 +0500

    youtube logo added

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -21,7 +21,8 @@ const navbar = () => {
                             <p className='dtl'><b>Follow Us</b></p>
                             <p><span><a href="https://www.facebook.com/delightrwanda" target={'_blank'}><i className="fa-brands fa-facebook-f"></i></a></span>
                                 <span><a href="https://www.twitter.com/DelightRwanda1" target={'_blank'}><i className="fa-brands fa-twitter"></i></a></span>
-                                <span><a href="https://www.instagram.com/delight_rwanda" target={'_blank'}><i className="fa-brands fa-instagram"></i></a></span></p>
+                                <span><a href="https://www.instagram.com/delight_rwanda" target={'_blank'}><i className="fa-brands fa-instagram"></i></a></span>
+                                <span><a href="https://www.youtube.com/@delightrwanda2232" target={'_blank'}><i className="fa-brands fa-youtube"></i></a></span></p>
                         </div>
                     </div>
                 </div>
