a8b2ca4aa1e54e68f826cf65da3a4722e16435a3
Fahad <85941321+fahadabdulqayyum16@users.noreply.github.com>
2023-03-19 15:48:02 +0500

    homepage text changed, img alignment, event's date adjusted

--- src/components/home.js
+++ src/components/home.js
@@ -8,9 +8,18 @@ const home = () => {
             <div className="container">
                 <h1>Always help poor & Deserved People</h1>
                 <p>
-                    Volunteers like you help make the world a
-                    better place with your time and efforts.
-                    Turn your compassion into action and volunteer.
+                    Join our team by donating any amount or in-kind gifts for street children of Kigali, Rwanda to
+                    afford daily free restaurant/food, clean drinking water, health care services, toilets, bath facilities,
+                    accommodation, mentorship, and takes them back to school. The number of children in our
+                    shelter for homeless children is still increasing year by year.<br />
+                    Since May 2022, Delight Rwanda has started a Shelter for Homeless Children project, which
+                    aims to improve the lives of street children by reintegrating them into families or foster care and
+                    supporting them to have access to education, supporting their families to acquire technical and
+                    vocational training for future self-reliance.<br />
+                    As a result, we need to support a big number so that we can easily accommodate such increasing
+                    of street children in our shelter. I hope that you will take our request into your consideration and
+                    will help us to make this work possible.<br />
+                    <b>Join our Initiative to change the life of street children</b>
                 </p>
                 <button>
                     <Link to={"/donationPage"}>Donate Now</Link>
